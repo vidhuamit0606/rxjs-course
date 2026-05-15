96b5817bffc0f07a57e7e5550f0a36e4660114aa
skamit <skvidusharma@gmail.com>
2026-05-15 11:40:29 +0530

    demoe -Unsubscription In Detail
    Fix typo in response handling and improve fetch with AbortController.

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,27 @@
 
+import {Observable} from 'rxjs';
 
+
+export function createHttpObservable(url:string) {
+    return Observable.create(observer => {
+     const controller = new AbortController();
+        const signal = controller.signal;
+      
+     fetch(`api/courses`,{signal})
+       .then(response => {
+            return reponse.json();
+       })
+       .then(body => {
+           observer.next(body);
+            observer.complete();
+       })
+      
+    });
+      .catch(err => {
+
+                observer.error(err);
+
+      });
+
+      controller.abort()
+}      
